d92cd49812d1c7fbef2af464899b2ad12644f5ef
Lawal <awwal1999@gmail.com>
2020-11-24 10:19:49 +0100

    Add column page ui

---
+++ resources/views/home.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="ui">
+{{--    <nav class="navbar app">App bar</nav>--}}
+    <nav class="navbar board">Project Board</nav>
+    <div class="columns">
+        <div class="column">
+            <header>Column 1</header>
+            <ul>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet metus laoreet, ut condimentum</li>
+            </ul>
+            <footer>Add a card...</footer>
+        </div>
+        <div class="column">
+            <header>Column 2</header>
+            <ul>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
+                <li>
+                    <img src="holder.js/300x150?auto=yes&bg=#ccc" alt="">
+                    Lorem ipsum dolor sit amet
+                </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet metus laoreet, ut condimentum</li>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet metus laoreet, ut condimentum</li>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
+                <li>Lorem ipsum dolor sit amet</li>
+            </ul>
+            <footer>Add a card...</footer>
+        </div>
+        <div class="column">
+            <header>Column 3</header>
+            <ul>
+                <li>
+                    <img src="holder.js/600x400?auto=yes&bg=#ccc" alt="">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
+                <li>
+                    <img src="holder.js/150x150?auto=yes&bg=#ccc" alt="">
+                    Lorem ipsum dolor sit amet
+                </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet metus laoreet, ut condimentum</li>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet metus laoreet, ut condimentum</li>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
+                <li>Lorem ipsum dolor sit amet</li>
+            </ul>
+            <footer>Add a card...</footer>
+        </div>
+        <div class="column">
+            <header>New</header>
+            <ul>
+                <li>Just some text</li>
+                <li>Just some text</li>
+            </ul>
+            <footer>Add a card...</footer>
+        </div>
+
+    </div>
+</div>
+@endsection
